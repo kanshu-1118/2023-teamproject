2a0a76068e93c68434a3f97adb61e121f4f7f849
下河寛宗 <kan.1118.shu@gmail.com>
2023-10-27 02:52:37 +0900

    path

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -5,34 +5,84 @@ import { navArray } from "../libs/nav";
 import { Box, Center, Flex } from "../../../styled-system/jsx";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { center, flex } from "../../../styled-system/patterns";
+import { useEffect, useState } from "react";
 
 export const appName = "Sample App"
 
-function Layout({ children } : any) {
+interface propsType {
+    id: "string",
+}
+
+function Layout({children} : any , props: propsType ) {
+    // console.log(location.pathname);
+
+    const [pathStatus,setPathStatus] = useState("/") 
+
+    useEffect(() => {
+        {(() : any => {
+            if (location.pathname == "/") {
+                setPathStatus("home")
+            }else if (location.pathname == "/post") {
+                setPathStatus("post")
+            }else if (location.pathname == "/timetable") {
+                setPathStatus("timetable")
+            }else if (location.pathname == "/mypage") {
+                setPathStatus("mypage")
+            }else {
+                setPathStatus("null")
+            }
+        })()}
+    },[])
+    
     return (
-        <div>
+        <div className={css({padding:"48px 0 100px"})}>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-        <header>
-            <h1>{appName}</h1>
-            <p>ここにはLayout.jsの内容が表示されています。</p>
-        </header>
-        <main>
-            { children }
-        </main>
-        <footer className={css({position:"fixed",bottom:"0",width:"100vw", bgColor:"main",})}>
-            <Flex >
-                {navArray.map((e:any,i:number) => {
-                    return(
-                        <Center padding={"14px 0"} gap={"8px"} key={i} flexDir={"column"} w={"calc(100vw/4)"} >
-                            <FontAwesomeIcon icon={e.image} className={css({color:"base",height:"20px"})} />
-                            <p className={css({fontSize:"10px", fontWeight:"bold", color:"base"})}>{e.name}</p>
-                        </Center>
-                    )
-                })}
-            </Flex>
-        </footer>
+            <header className={center({position:"fixed",top:"0",width:"100vw", bgColor:"main",padding:"10px 0"})}>
+                <p className={css({fontSize:"20px",fontWeight:"bold",color:"base"})}>
+                    {/* {Cの条件 ? (Bの条件 ? (Aの条件 ? (<p>A</p>):(<p>B</p>)) :(<p>C</p>)) :(<p>D</p>)} */}
+                    {/* {true ? (true ? (false ? (false ? ("A"):("B")):("C")) :("D")) :("エラー")} */}
+                    {(() : any => {
+                        if (pathStatus == "home") {
+                            return "ホーム"
+                        }else if (pathStatus == "post") {
+                            return "投稿"
+                        }else if (pathStatus == "timetable") {
+                            return "時刻表"
+                        }else if (pathStatus == "mypage") {
+                            return "マイページ"
+                        }else {
+                            return "存在しないページ"
+                        }
+                    })()}
+                </p>
+            </header>
+            <main>
+                { children }
+            </main>
+            <footer className={css({position:"fixed",bottom:"0",width:"100vw", bgColor:"main",})}>
+                <Flex >
+                    {navArray.map((e:any,i:number) => {
+                        if (e.id == pathStatus) {
+                            return(
+                                <Center padding={"14px 0 20px"} gap={"8px"} key={i} flexDir={"column"} w={"calc(100vw/4)"} >
+                                    <FontAwesomeIcon icon={e.image} className={css({color:"accent",height:"20px"})} />
+                                    <p className={css({fontSize:"10px", fontWeight:"bold", color:"base"})}>{e.name}</p>
+                                </Center>
+                            )
+                        }else {
+                            return(
+                                <Center padding={"14px 0 20px"} gap={"8px"} key={i} flexDir={"column"} w={"calc(100vw/4)"} >
+                                    <FontAwesomeIcon icon={e.image} className={css({color:"base",height:"20px"})} />
+                                    <p className={css({fontSize:"10px", fontWeight:"bold", color:"base"})}>{e.name}</p>
+                                </Center>
+                            )
+                        }
+                    })}
+                </Flex>
+            </footer>
         </div>
     );
 }
--- src/pages/libs/nav.ts
+++ src/pages/libs/nav.ts
@@ -5,25 +5,25 @@ const navArray : navType = [
     {   
         id:"home",
         name: "ホーム",
-        link : "#",
+        link : "/",
         image: faHouse,
     },
     {
         id:"post",
         name: "投稿",
-        link : "#",
+        link : "/post",
         image: faPenSquare,
     },
     {
-        id:"time",
+        id:"timetable",
         name: "時刻表",
-        link : "#",
+        link : "/timetable",
         image: faClock,
     },
     {
         id:"mypage",
         name: "マイページ",
-        link : "#",
+        link : "/link",
         image: faUser,
     },
 ]
