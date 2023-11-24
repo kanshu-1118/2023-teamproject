8b0782c262cbd2070fea324d24d1552a7fb39276
森尾　優菜 <>
2023-11-25 00:13:23 +0900

    [add]mypage更新

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,6 +1,6 @@
 import Layout from '@/components/Layout'
 import React from 'react'
-import { flex } from '../../styled-system/patterns'
+import { center, flex } from '../../styled-system/patterns'
 import { css } from '../../styled-system/css'
 
 const Mypage = () => {
@@ -9,27 +9,29 @@ const Mypage = () => {
             <div>
 
                 <div className={flex({justifyContent:"space-between", marginTop:"35px"})}>
-                    <h1 className={css({marginLeft:"25px", fontSize:"2rem", fontWeight:"bold"})}>ss_miyu</h1>
-                    <img className={css({marginRight:"15px"})} src="" alt="設定" />
+                    <h1 className={css({marginLeft:"25px", fontSize:"20px", fontWeight:"bold"})}>ss_miyu</h1>
+                    <img className={css({marginRight:"15px", w:"24px", h:"21px"})} src="/images/gear.png" alt="設定" />
                 </div>
                 
-                <div className={flex({flexDir:"column"})}>
-                    <img src="" alt="icon" />
-                    <p>島田美優</p>
-                    <p>1997.11.11</p>
-                    <p>大阪</p>
+                <div className={flex({marginBottom:"60px"})}>
+                    <img className={css({margin:"0 25px"})} src="/images/icon.png" alt="icon" />
+                    <div className={flex({flexDir:"column", fontWeight:"bold"})}>
+                        <p className={css({margin:"10px 0 15px", fontSize:"14px"})}>島田美優</p>
+                        <p className={css({marginBottom:"5px", fontSize:"10px"})}>1997.11.11</p>
+                        <p className={css({fontSize:"10px"})}>大阪</p>
+                    </div>
                 </div>
 
-                <div>
-                    <h2 className={css({textAlign:"center"})}>マイルート</h2>
+                <div className={css({marginBottom:"50px"})}>
+                    <h2 className={css({textAlign:"center", fontSize:"20px", fontWeight:"bold"})}>マイルート</h2>
                 </div>
 
-                <div className={css({textAlign:"center"})}>
-                    <h2>におい登録</h2>
-                    <div className={css({border:"solid 1px "})}>
-                        <p>タバコ</p>
-                        <p>汗が参加したにおい</p>
-                        <p>口臭</p>
+                <div className={css({textAlign:"center", fontWeight:"bold"})}>
+                    <h2 className={css({fontSize:"20px", margin:"25px"})}>におい登録</h2>
+                    <div className={center({flexDir:"column", gap:"10px", textAlign:"center", fontSize:"10px"})}>
+                        <p className={center({border:"solid 1px",  w:"250px", h:"30px", borderRadius:"10px"})}>タバコ</p>
+                        <p className={center({border:"solid 1px",  w:"250px", h:"30px", borderRadius:"10px"})}>汗が参加したにおい</p>
+                        <p className={center({border:"solid 1px",  w:"250px", h:"30px", borderRadius:"10px"})}>口臭</p>
                     </div>
                 </div>
 
