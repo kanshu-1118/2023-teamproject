4422edf0a6dad071aeffa4099c477a3390d41e07
下河寛宗 <kan.1118.shu@gmail.com>
2023-11-24 11:31:30 +0900

    [add] timetablePages作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps } : AppProps) {
   return (
     <AnimatePresence>
       <Component {...pageProps} />
