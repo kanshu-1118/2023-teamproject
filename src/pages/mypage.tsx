f572c755c528d2b30f202a4b174000733237a04d
森尾　優菜 <>
2023-11-27 22:59:04 +0900

    mypage更新

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -9,7 +9,7 @@ const Mypage = () => {
             <div>
 
                 <div className={flex({justifyContent:"space-between", marginTop:"35px"})}>
-                    <h1 className={css({marginLeft:"25px", fontSize:"20px", fontWeight:"bold"})}>ss_miyu</h1>
+                    <h1 className={css({margin:"0 0 10px 25px", fontSize:"20px", fontWeight:"bold"})}>ss_miyu</h1>
                     <img className={css({marginRight:"15px", w:"24px", h:"21px"})} src="/images/gear.png" alt="設定" />
                 </div>
                 
@@ -22,12 +22,31 @@ const Mypage = () => {
                     </div>
                 </div>
 
-                <div className={css({marginBottom:"50px"})}>
-                    <h2 className={css({textAlign:"center", fontSize:"20px", fontWeight:"bold"})}>マイルート</h2>
+                <div>
+                    <h2 className={css({textAlign:"center",marginBottom:"25px", fontSize:"20px", fontWeight:"bold"})}>マイルート</h2>
+
+                    <div>
+                    <div className={flex({})}>
+                        <img className={css({margin:"15px 0 36px"})} src="/images/line.png" alt="" />
+                        <div className={flex({position:"absolute"})}>
+                            <p className={center({w:"50px", h:"20px", bgColor:"#fff", border:"1px solid", borderRadius:"5px", margin:"auto 18px auto 0", fontSize:"10px", fontWeight:"bold"})}>発車</p>
+                            <p className={center({w:"140px", h:"40px", bgColor:"#fff", border:"1px solid", borderRadius:"5px", marginRight:"18px", fontSize:"14px", fontWeight:"bold"})}>阪急高槻駅</p>
+                            <p className={center({w:"140px", h:"40px", bgColor:"#fff",border:"1px solid", borderRadius:"5px", fontSize:"14px", fontWeight:"bold"})}>7:00</p>
+                        </div>
+                    </div>
+                    <div className={flex({})}>
+                    <img className={css({margin:"15px 0 36px"})} src="/images/line.png" alt="" />
+                        <div className={flex({position:"absolute"})}>
+                            <p className={center({w:"50px", h:"20px", bgColor:"#fff", border:"1px solid", borderRadius:"5px", margin:"auto 18px auto 0", fontSize:"10px", fontWeight:"bold"})}>発車</p>
+                            <p className={center({w:"140px", h:"40px", bgColor:"#fff", border:"1px solid", borderRadius:"5px", marginRight:"18px", fontSize:"14px", fontWeight:"bold"})}>大阪梅田駅</p>
+                            <p className={center({w:"140px", h:"40px", bgColor:"#fff",border:"1px solid", borderRadius:"5px", fontSize:"14px", fontWeight:"bold"})}>7:28</p>
+                        </div>
+                    </div>
+                    </div>
                 </div>
 
                 <div className={css({textAlign:"center", fontWeight:"bold"})}>
-                    <h2 className={css({fontSize:"20px", margin:"25px"})}>におい登録</h2>
+                    <h2 className={css({fontSize:"20px", margin:"30px 0 25px"})}>におい登録</h2>
                     <div className={center({flexDir:"column", gap:"10px", textAlign:"center", fontSize:"10px"})}>
                         <p className={center({border:"solid 1px",  w:"250px", h:"30px", borderRadius:"10px"})}>タバコ</p>
                         <p className={center({border:"solid 1px",  w:"250px", h:"30px", borderRadius:"10px"})}>汗が参加したにおい</p>
